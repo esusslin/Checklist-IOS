49d47fcc16deeda398b6237cb618da2f24224232
Emmet Susslin <esusslin@gmail.com>
2016-06-09 10:56:48 -0400

    checklist item added & refactored

---
+++ Checklist-IOS/Checklist-IOS/ChecklistItem.swift
@@ -0,0 +1,18 @@
+//
+//  ChecklistItem.swift
+//  Checklist-IOS
+//
+//  Created by Emmet Susslin on 6/9/16.
+//  Copyright © 2016 Emmet Susslin. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+    func toggleChecked() {
+        checked = !checked
+    }
+}
+
