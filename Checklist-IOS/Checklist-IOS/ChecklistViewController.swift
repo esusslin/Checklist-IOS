0f215cb0ebc23287b87e697aa3969a48d9a9ab76
Emmet Susslin <esusslin@gmail.com>
2016-06-09 06:25:22 -0400

    table cells with user-reactive checks functional

--- Checklist-IOS/Checklist-IOS/ChecklistViewController.swift
+++ Checklist-IOS/Checklist-IOS/ChecklistViewController.swift
@@ -52,31 +52,42 @@ class ChecklistViewController: UITableViewController {
         } else if indexPath.row == 4 {
             label.text = row4text
         }
+        configureCheckmarkForCell(cell, indexPath: indexPath)
         return cell
     }
     
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    override func tableView(tableView: UITableView,
+                            didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
         if let cell = tableView.cellForRowAtIndexPath(indexPath) {
-            var isChecked = false
-            if indexPath.row == 0 { row0checked = !row0checked
-                isChecked = row0checked
-            } else if indexPath.row == 1 { row1checked = !row1checked
-                isChecked = row1checked
+            if indexPath.row == 0 {
+              row0checked = !row0checked
+            } else if indexPath.row == 1 {
+              row1checked = !row1checked
             } else if indexPath.row == 2 {
-                row2checked = !row2checked
-                isChecked = row2checked
+              row2checked = !row2checked
             } else if indexPath.row == 3 {
-                row3checked = !row3checked
-                isChecked = row3checked
+              row3checked = !row3checked
             } else if indexPath.row == 4 {
-                row4checked = !row4checked
-                isChecked = row4checked }
-            if isChecked {
-                cell.accessoryType = .Checkmark
-            } else {
-                cell.accessoryType = .None
-            } }
+              row4checked = !row4checked
+            }
+            configureCheckmarkForCell(cell, indexPath: indexPath)
+        }
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
+    
+    func configureCheckmarkForCell(cell: UITableViewCell, indexPath: NSIndexPath) {
+        var isChecked = false
+        if indexPath.row == 0 { isChecked = row0checked
+        } else if indexPath.row == 1 { isChecked = row1checked
+        } else if indexPath.row == 2 { isChecked = row2checked
+        } else if indexPath.row == 3 { isChecked = row3checked
+        } else if indexPath.row == 4 { isChecked = row4checked
+        }
+        if isChecked {
+            cell.accessoryType = .Checkmark
+        } else {
+            cell.accessoryType = .None
+        } }
 }
 
